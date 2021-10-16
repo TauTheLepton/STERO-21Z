3da20a7dee0798de63cde6fe289ecbcb37215df1
ZubikLena <80119227+ZubikLena@users.noreply.github.com>
2021-10-16 20:53:54 +0200

    Problem with quaterions and relative positioning resolved.

--- scripts/kinematic2.py
+++ scripts/kinematic2.py
@@ -9,7 +9,7 @@ import tf
 linear = 0.11
 angular = 0.6
 eps = 0.1
-# global callback_data_x, callback_data_y, callback_data_orient
+
 callback_data_x = 0.0
 callback_data_y = 0.0
 callback_data_orient = 0.0
@@ -18,7 +18,7 @@ def odom_callback(odometry):
    global callback_data_x, callback_data_y, callback_data_orient
    callback_data_x = odometry.pose.pose.position.x
    callback_data_y = odometry.pose.pose.position.y
-   callback_data_orient = tf.transformations.euler_from_quaternion((0, 0, odometry.pose.pose.orientation.z, 0))[2]
+   callback_data_orient = tf.transformations.euler_from_quaternion((0, 0, odometry.pose.pose.orientation.z, odometry.pose.pose.orientation.w))[2]
 
 
 def talker(side):
@@ -27,35 +27,31 @@ def talker(side):
    rospy.init_node('kinematic', anonymous=True)
    rate = rospy.Rate(10) # 10hz 0.1s
    vel_msg = Twist()
-   goal_pose_x = callback_data_x + side
-   goal_pose_y = callback_data_y
-   goal_orinet = callback_data_orient
-   print(callback_data_orient)
+   goal_pose_x = callback_data_x + side * math.cos(callback_data_orient)
+   goal_pose_y = callback_data_y + side * math.sin(callback_data_orient)
+   goal_orient = callback_data_orient + math.pi/2
 
    print("Let's move your ass!")
+   count = 4
   
-   while not rospy.is_shutdown():
+   while not rospy.is_shutdown() and count > 0:
       while math.sqrt((goal_pose_x - callback_data_x)**2 + (goal_pose_y - callback_data_y)**2) > eps:
-         print(math.sqrt((goal_pose_x - callback_data_x)**2 + (goal_pose_y - callback_data_y)**2))
+         print("goal", goal_pose_x, goal_pose_y)
+         print("actual", callback_data_x, callback_data_y)
          vel_msg.linear.x = linear
          vel_msg.angular.z = 0
          pub.publish(vel_msg)
          rate.sleep()
-      goal_orinet += math.pi/2
-      while abs(abs(goal_orinet) - abs(callback_data_orient)) > eps:
-         print(abs(abs(goal_orinet) - abs(callback_data_orient)))
-         print(callback_data_orient)
+      while (goal_orient - callback_data_orient) > eps:
+         print(goal_orient - callback_data_orient)
          vel_msg.linear.x = 0
          vel_msg.angular.z = angular
          pub.publish(vel_msg)
          rate.sleep()
-      # for i in range(iters_ang):
-      #    vel_msg.linear.x = 0
-      #    vel_msg.angular.z = angular
-      #    pub.publish(vel_msg)
-      #    rate.sleep()
-
-      # count -= 1
+      goal_pose_x = callback_data_x + side * math.cos(callback_data_orient)
+      goal_pose_y = callback_data_y + side * math.sin(callback_data_orient)
+      goal_orient += math.pi/2
+      count -= 1
 
 
 if __name__ == '__main__':
