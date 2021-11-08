79b72b76fb9ac779209eddec50baf81ee0026ae0
TauTheLepton <matipalczuk@gmail.com>
2021-11-08 02:26:11 +0100

    added some control code, need params

--- scripts/navi.cpp
+++ scripts/navi.cpp
@@ -5,41 +5,64 @@
 #include "dwa_local_planner/dwa_planner_ros.h"
 #include "global_planner/planner_core.h"
 #include <sstream>
+#include "geometry_msgs/PoseStamped.h"
+#include "rotate_recovery/rotate_recovery.h"
+
+void my_callback(const std_msgs::StringConstPtr& str)
+{
+    double i=1;
+    printf("Current i = %2.2f", i);
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "navi");
     ros::NodeHandle n;
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+    ros::Publisher pub_vel = n.advertise<geometry_msgs::Twist>("topic", 1000); // set the right topic to publish velocity
+    // ros::Subscriber sub = n.subscribe("some_topic", 1000, my_callback);
     ros::Rate loop_rate(10);
+
+    std::vector<geometry_msgs::PoseStamped> plan;
+    geometry_msgs::PoseStamped pose_start;
+    geometry_msgs::PoseStamped pose_goal;
+    geometry_msgs::Twist vel;
+
+    bool reached_goal = false;
 
     // sth here didn't want to work, and I had to paste this line to command line:
     // sudo ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
     tf2_ros::Buffer buf(ros::Duration(10));
-    tf2_ros::TransformListener tf(buf);
+    tf2_ros::TransformListener tf(buf); // idk if this is needed
     costmap_2d::Costmap2DROS costmap_local("costmap_local", buf);
     costmap_2d::Costmap2DROS costmap_global("costmap_global", buf);
 
-    dwa_local_planner::DWAPlannerROS dp;
-    dp.initialize("my_dwa_planner", &buf, &costmap_local);
+    rotate_recovery::RotateRecovery recovery;
+    recovery.initialize("recovery", &buf, &costmap_global, &costmap_local);
 
-    global_planner::GlobalPlanner planner_global("planner_global", costmap_global.getCostmap(), "map");
+    dwa_local_planner::DWAPlannerROS planner_local;
+    planner_local.initialize("planner_local", &buf, &costmap_local);
+
+    global_planner::GlobalPlanner planner_global;
+    planner_global.initialize("planner_global", &costmap_global);
+
+    planner_global.makePlan(pose_start, pose_goal, plan);
+    planner_local.setPlan(plan);
 
     int count = 0;
     while (ros::ok())
     {
-        std_msgs::String msg;
-
-        std::stringstream ss;
-        ss << "hello world " << count;
-        msg.data = ss.str();
-
-        ROS_INFO("%s", msg.data.c_str());
-
-        chatter_pub.publish(msg);
+        if (!reached_goal) {
+            reached_goal = planner_local.isGoalReached();
+            bool found_trajectory = planner_local.computeVelocityCommands(vel); // could use dwaComputeVelocityCommands(), but needs 2 arguments
+            if (found_trajectory) {
+                pub_vel.publish(vel);
+            } else {
+                recovery.runBehavior();
+            }
+        }
+        // ROS_INFO("%s", msg.data.c_str());
 
         ros::spinOnce();
-
         loop_rate.sleep();
         ++count;
     }
