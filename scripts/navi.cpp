755b867682994c1394d71aa57aba0c8dbae4c422
TauTheLepton <matipalczuk@gmail.com>
2021-11-10 11:08:30 +0100

    minor changes and added comments

--- scripts/navi.cpp
+++ scripts/navi.cpp
@@ -9,21 +9,27 @@
 #include "rotate_recovery/rotate_recovery.h"
 #include "nav_msgs/Odometry.h"
 
+// definition of poses
 nav_msgs::Odometry pose_current;
 geometry_msgs::PoseStamped pose_goal;
+bool received_goal = false;
 
+// callback for sub_current
 void get_current_pose_callback(const nav_msgs::Odometry::ConstPtr& pose)
 {
     pose_current = *pose;
-    pose_current.header.frame_id = "map";
+    pose_current.header.frame_id = "map"; // change frame_id to "map" but not shure if it is right
 }
 
+// callback for sub_goal
 void get_goal_callback(const geometry_msgs::PoseStamped::ConstPtr& pose)
 {
     pose_goal = *pose;
-    pose_goal.header.frame_id = "map";
+    pose_goal.header.frame_id = "map"; // change frame_id to "map" but not shure if it is right
+    received_goal = true;
 }
 
+// alternative callback for sub_goal
 // void goal_callback(const geometry_msgs::PoseStamped::SharedPtr msg){
 //     // ROS_INFO_STREAM("Received pose: " << msg);
 //     // msg->pose.pose.position.x
@@ -60,6 +66,7 @@ void get_goal_callback(const geometry_msgs::PoseStamped::ConstPtr& pose)
 
 // }
 
+// onversion from type nav_msgs::Odometry to geometry_msgs::PoseStamped
 geometry_msgs::PoseStamped odom2pose(nav_msgs::Odometry odom)
 {
     geometry_msgs::PoseStamped my_pose;
@@ -76,37 +83,43 @@ geometry_msgs::PoseStamped odom2pose(nav_msgs::Odometry odom)
     return my_pose;
 }
 
+// main
 int main(int argc, char **argv)
 {
+    // initialization of node
     ros::init(argc, argv, "navi");
     ros::NodeHandle n;
+    ros::Rate loop_rate(10);
 
+    // setting up publishers and subscribers
     ros::Publisher pub_vel = n.advertise<geometry_msgs::Twist>("key_vel", 1000);
     ros::Subscriber sub_current = n.subscribe("/mobile_base_controller/odom", 1000, get_current_pose_callback);
     ros::Subscriber sub_goal = n.subscribe("/move_base_simple/goal", 1000, get_goal_callback);
-    ros::Rate loop_rate(10);
 
+    // initialization fo vatriables needed for planning and executing route
     geometry_msgs::PoseStamped pose_start;
     std::vector<geometry_msgs::PoseStamped> plan;
-
     geometry_msgs::Twist vel;
-
     bool reached_goal = true;
+    bool is_going= false;
 
     // sth here didn't want to work, and I had to paste this line to command line:
     // sudo ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
+    // setting up costmaps
     tf2_ros::Buffer buf(ros::Duration(10));
     tf2_ros::TransformListener tf(buf); // idk if this is needed
     costmap_2d::Costmap2DROS costmap_local("costmap_local", buf);
     costmap_2d::Costmap2DROS costmap_global("costmap_global", buf);
 
+    // rcovery behavior for when local planner gets stuck
     rotate_recovery::RotateRecovery recovery;
     recovery.initialize("recovery", &buf, &costmap_global, &costmap_local);
 
+    // initialization of local planer
     dwa_local_planner::DWAPlannerROS planner_local;
     planner_local.initialize("planner_local", &buf, &costmap_local);
 
-    // idk which initialization for GlobalPlanner is better, but as for now neither work
+    // initialization of global planer, idk which is better is better/the right one
 
     // global_planner::GlobalPlanner planner_global;
     // planner_global.initialize("planner_global", &costmap_global);
@@ -116,23 +129,36 @@ int main(int argc, char **argv)
     int count = 0;
     while (ros::ok())
     {
-        if (reached_goal) {
-            // pose_start = pose_current;
-            pose_start.header.frame_id = "map";
-            pose_start = odom2pose(pose_current);
-            planner_global.makePlan(pose_start, pose_goal, plan);
-            planner_local.setPlan(plan);
+        if (!is_going) {
+            // happens when goal is reached, so there is no new goal
+            if (received_goal) {
+                // if new one is given, in this part the plan will be generated
+                pose_start = odom2pose(pose_current); // convert pose_current which is Odometry to PoseStamped
+                pose_start.header.frame_id = "map"; // change frame_id to "map" but not shure if it is right
+                planner_global.makePlan(pose_start, pose_goal, plan); // global planner makes plan
+                bool plan_status = planner_local.setPlan(plan); // plan from global planner is passed to the local planner
+                if (plan_status) { // could add more conditions like 'did_make_plan' or sth
+                    // when everything was calculated correctly moves on to moving
+                    is_going = true;
+                    received_goal = false;
+                }
+            }
         } else {
-            reached_goal = planner_local.isGoalReached();
+            // check if local planner found a trajectory
             bool found_trajectory = planner_local.computeVelocityCommands(vel); // could use dwaComputeVelocityCommands(), but needs 2 arguments
             if (found_trajectory) {
+                // if it did publish instructions
                 pub_vel.publish(vel);
             } else {
+                // if it didn't run recovery
                 recovery.runBehavior();
             }
+            reached_goal = planner_local.isGoalReached(); // check if local planner has reached a goal
+            if (reached_goal) is_going = false;
         }
         // ROS_INFO("%s", msg.data.c_str());
 
+        // basics needed for publishing/subscribing and while loop tooperate smoothly
         ros::spinOnce();
         loop_rate.sleep();
         ++count;
