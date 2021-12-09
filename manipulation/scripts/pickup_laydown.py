f155b881cbd76e4059e30fa18e0439e53106de90
TauTheLepton <matipalczuk@gmail.com>
2021-12-09 01:01:48 +0100

    added object distance checking and some refactoring

--- manipulation/scripts/pickup_laydown.py
+++ manipulation/scripts/pickup_laydown.py
@@ -11,7 +11,65 @@ from control_msgs.msg import FollowJointTrajectoryResult
 from rcprg_planner import *
 from rcprg_ros_utils import exitError
 
-if __name__ == "__main__":
+#Define a function for frequently used routine in this test
+def planAndExecute(velma, p, q_dest):
+    print "Planning motion to the goal position using set of all joints..."
+    print "Moving to valid position, using planned trajectory."
+    goal_constraint = qMapToConstraints(q_dest, 0.01, group=velma.getJointGroup("impedance_joints"))
+    for i in range(5):
+        rospy.sleep(0.5)
+        js = velma.getLastJointState()
+        print "Planning (try", i, ")..."
+        traj = p.plan(js[1], [goal_constraint], "impedance_joints", max_velocity_scaling_factor=0.15, planner_id="RRTConnect")
+        if traj == None:
+            continue
+        print "Executing trajectory..."
+        if not velma.moveJointTraj(traj, start_time=0.5):
+            exitError(5)
+        if velma.waitForJoint() == 0:
+            break
+        else:
+            print "The trajectory could not be completed, retrying..."
+            continue
+    rospy.sleep(0.5)
+    js = velma.getLastJointState()
+    if not isConfigurationClose(q_dest, js[1]):
+        exitError(6)
+
+def makeCimpMove(velma, Tf_frame, announcement):
+    print "Switch to cart_imp mode (no trajectory)..."
+    if not velma.moveCartImpRightCurrentPos(start_time=0.2):
+        exitError(10)
+    if velma.waitForEffectorRight() != 0:
+        exitError(11)
+
+    rospy.sleep(0.5)
+
+    diag = velma.getCoreCsDiag()
+    if not diag.inStateCartImp():
+        exitError(12, msg="The core_cs should be in cart_imp state, but it is not")
+
+    print announcement
+    if not velma.moveCartImpRight([Tf_frame], [3.0], None, None, None, None, PyKDL.Wrench(PyKDL.Vector(5,5,5), PyKDL.Vector(5,5,5)), start_time=0.5):
+        exitError(13)
+    if velma.waitForEffectorRight() != 0:
+        exitError(14)
+    rospy.sleep(0.5)
+    print "Calculating difference between desiread and reached pose..."
+    T_B_T_diff = PyKDL.diff(Tf_frame, Tf_frame, 1.0)
+    print T_B_T_diff
+    if T_B_T_diff.vel.Norm() > 0.05 or T_B_T_diff.rot.Norm() > 0.05:
+        exitError(15)
+
+# check if object is close enough to robot
+def isCloseEnough(object_pose, robot_pose):
+    s = math.sqrt((object_pose[0] - robot_pose[0]) ** 2 + (object_pose[1] - robot_pose[1]) ** 2 + (object_pose[2] - robot_pose[2]) ** 2)
+    if s < 1.2:
+        return True
+    else:
+        return False
+
+def main():
     #Define some configurations
 
     q_start = {'torso_0_joint':6.067961977395732e-07, 'right_arm_0_joint':-0.2892, 'right_arm_1_joint':-1.8185, 'right_arm_2_joint':1.2490,
@@ -63,56 +121,6 @@ if __name__ == "__main__":
         exitError(2, msg="Could not initialize Planner")
     print "Planner initialization ok!"
 
-    #Define a function for frequently used routine in this test
-    def planAndExecute(q_dest):
-        print "Planning motion to the goal position using set of all joints..."
-        print "Moving to valid position, using planned trajectory."
-        goal_constraint = qMapToConstraints(q_dest, 0.01, group=velma.getJointGroup("impedance_joints"))
-        for i in range(5):
-            rospy.sleep(0.5)
-            js = velma.getLastJointState()
-            print "Planning (try", i, ")..."
-            traj = p.plan(js[1], [goal_constraint], "impedance_joints", max_velocity_scaling_factor=0.15, planner_id="RRTConnect")
-            if traj == None:
-                continue
-            print "Executing trajectory..."
-            if not velma.moveJointTraj(traj, start_time=0.5):
-                exitError(5)
-            if velma.waitForJoint() == 0:
-                break
-            else:
-                print "The trajectory could not be completed, retrying..."
-                continue
-        rospy.sleep(0.5)
-        js = velma.getLastJointState()
-        if not isConfigurationClose(q_dest, js[1]):
-            exitError(6)
-
-    def makeCimpMove(Tf_frame, announcement):
-        print "Switch to cart_imp mode (no trajectory)..."
-        if not velma.moveCartImpRightCurrentPos(start_time=0.2):
-            exitError(10)
-        if velma.waitForEffectorRight() != 0:
-            exitError(11)
-    
-        rospy.sleep(0.5)
-    
-        diag = velma.getCoreCsDiag()
-        if not diag.inStateCartImp():
-            exitError(12, msg="The core_cs should be in cart_imp state, but it is not")
-
-        print announcement
-        if not velma.moveCartImpRight([Tf_frame], [3.0], None, None, None, None, PyKDL.Wrench(PyKDL.Vector(5,5,5), PyKDL.Vector(5,5,5)), start_time=0.5):
-            exitError(13)
-        if velma.waitForEffectorRight() != 0:
-            exitError(14)
-        rospy.sleep(0.5)
-        print "Calculating difference between desiread and reached pose..."
-        T_B_T_diff = PyKDL.diff(Tf_frame, Tf_frame, 1.0)
-        print T_B_T_diff
-        if T_B_T_diff.vel.Norm() > 0.05 or T_B_T_diff.rot.Norm() > 0.05:
-            exitError(15)
-
 
     #Loading octomap
     oml = OctomapListener("/octomap_binary")
@@ -137,11 +145,35 @@ if __name__ == "__main__":
     T_B_Table_b = velma.getTf("B", "table_b")
     T_B_Jar = velma.getTf("B", "jar")
     T_B_Bowl = velma.getTf("B", "bowl")
+    T_B_torso = velma.getTf("B", "torso")
+    T_B_arm1 = velma.getTf("B", "arm1")
+    T_B_base = velma.getTf("B", "base")
     #Lookup where objects are located
     # print T_B_Table_a.M, T_B_Table_a.p
     # print T_B_Table_b.M, T_B_Table_b.p
     # print T_B_Jar.M, T_B_Jar.p
     # print T_B_Bowl.M, T_B_Bowl.p
+
+    orig_orient = T_B_base.M
+    orig_pose = [T_B_base.p[0], T_B_base.p[1], T_B_arm1.p[2]]
+    orig_base_rpy = T_B_base.M.GetRPY()
+
+    # check if can reach
+    robot_pose = [T_B_torso.p[0], T_B_torso.p[1], T_B_arm1.p[2]]
+    if isCloseEnough(T_B_Jar.p, robot_pose):
+        print "OBJECT IN REACH, PROCEEDING!!!"
+    else:
+        print "OBJECT IS TOO FAR AWAY!!!"
+        print "EXITING..."
+        return 0
+
+    # if T_B_Jar.p[0] > T_B_base.p[0]:
+    #     temp = T_B_Jar.p[0] > T_B_base.p[0]
+    # else:
+    #     temp = T_B_Jar.p[0] > T_B_base.p[0]
+    my_z_rotation = math.atan2(T_B_base.p[0] - T_B_Jar.p[0], T_B_base.p[1] - T_B_Jar.p[1])
+    # my_z_rotation = math.atan2(T_B_Jar.p[0], T_B_Jar.p[1])
+    goal_orient_pickup = PyKDL.Rotation.RPY(orig_base_rpy[0], orig_base_rpy[1], my_z_rotation)
 
     print "Checking if the starting configuration is as expected..."
     rospy.sleep(0.5)
@@ -149,16 +181,29 @@ if __name__ == "__main__":
 
     
     print "MOVING TO START POSITION!!!"
-    planAndExecute(q_start)
+    planAndExecute(velma, p, q_start)
 
 
     print "MOVING TO INTERMEDIATE POSITION!!!"
-    planAndExecute(q_pre_pickup)
+    planAndExecute(velma, p, q_pre_pickup)
 
 
     #Grip offset
-    T_B_Jar = PyKDL.Frame(T_B_Jar.M, PyKDL.Vector( T_B_Jar.p[0] - 0.25, T_B_Jar.p[1], T_B_Jar.p[2] + 0.1))
-    makeCimpMove(T_B_Jar, "MOVING TOWARDS THE JAR!!!")
+    # print "T_B_Jar"
+    # print T_B_Jar
+    # print "T_B_Jar.M"
+    # print T_B_Jar.M
+    # print "T_B_Jar.p"
+    # print T_B_Jar.p
+
+    # print "T_B_Table_a.M"
+    # print T_B_Table_a.M
+    # print "T_B_Table_a.p"
+    # print T_B_Table_a.p
+
+    # T_B_Jar = PyKDL.Frame(T_B_Jar.M, PyKDL.Vector( T_B_Jar.p[0] - 0.25, T_B_Jar.p[1], T_B_Jar.p[2] + 0.1))
+    T_B_Jar = PyKDL.Frame(goal_orient_pickup, PyKDL.Vector( T_B_Jar.p[0] - 0.25, T_B_Jar.p[1], T_B_Jar.p[2] + 0.1))
+    makeCimpMove(velma, T_B_Jar, "MOVING TOWARDS THE JAR!!!")
 
 
     # HERE SHOULD CLOSE GRIP
@@ -169,7 +214,7 @@ if __name__ == "__main__":
 
     #Grip offset
     T_B_Jar_up = PyKDL.Frame(T_B_Jar.M, PyKDL.Vector( T_B_Jar.p[0], T_B_Jar.p[1], T_B_Jar.p[2] + 0.3))
-    makeCimpMove(T_B_Jar_up, "MOVING WITH AN OBJECT UPWARDS")
+    makeCimpMove(velma, T_B_Jar_up, "MOVING WITH AN OBJECT UPWARDS")
 
 
     print "Switch to jnt_imp mode (no trajectory)..."
@@ -181,7 +226,7 @@ if __name__ == "__main__":
     
     
     print "MOVING JAR TO FINAL POSITION!!!"
-    planAndExecute(q_before_laydown)
+    planAndExecute(velma, p, q_before_laydown)
 
 
     print "Switch to cart_imp mode (no trajectory)..."
@@ -207,7 +252,7 @@ if __name__ == "__main__":
     T_B_Wr_down = velma.getTf("B", "Wr")
     #Grip offset
     T_B_Wr_down = PyKDL.Frame(T_B_Wr_down.M, PyKDL.Vector( T_B_Wr_down.p[0], T_B_Wr_down.p[1], T_B_Wr_down.p[2] + 0.3))
-    makeCimpMove(T_B_Wr_down, "DISTANCING FROM THE JAR")
+    makeCimpMove(velma, T_B_Wr_down, "DISTANCING FROM THE JAR")
 
 
     print "Switch to jnt_imp mode (no trajectory)..."
@@ -218,9 +263,13 @@ if __name__ == "__main__":
         exitError(4)
     
     print "MOVING TO START POSITION!!!"
-    planAndExecute(q_start)
+    planAndExecute(velma, p, q_start)
 
 
     print "ENDING OPERATION!!!"
 
     rospy.sleep(1.0)
+    return 0
+
+if __name__ == "__main__":
+    main()
